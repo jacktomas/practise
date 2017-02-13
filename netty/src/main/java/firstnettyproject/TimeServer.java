913e1975cfcbabf599de2008982710a534cbbb4e
wenda <1538417087@qq.com>
2017-02-13 19:03:48 +0800

    修改类名

--- netty/src/main/java/firstnettyproject/TimeServer.java
+++ netty/src/main/java/firstnettyproject/TimeServer.java
@@ -4,6 +4,7 @@ import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
@@ -26,12 +27,16 @@ public class TimeServer {
     }
 
     private void bind(int port) {
-        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
-        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
-        ServerBootstrap b = new ServerBootstrap();
-        b.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class).
-                option(ChannelOption.SO_BACKLOG, 1024).childHandler(new ChildChannelHandler());
+        EventLoopGroup bossGroup = new NioEventLoopGroup();
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
         try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class).
+                    option(ChannelOption.SO_BACKLOG, 1024).
+                    childHandler(new ChildChannelHandler());
+
             ChannelFuture f = b.bind(port).sync();
             f.channel().closeFuture().sync();
         } catch (InterruptedException e) {

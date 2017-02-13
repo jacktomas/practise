ea5b2ccecdfaf7623205653f96b9c0eaf5323285
wenda <1538417087@qq.com>
2017-02-13 18:59:18 +0800

    修改了类名

--- netty/src/main/java/firstnettyproject/TimeClient.java
+++ netty/src/main/java/firstnettyproject/TimeClient.java
@@ -4,6 +4,7 @@ import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
@@ -14,7 +15,7 @@ import io.netty.channel.socket.nio.NioSocketChannel;
 public class TimeClient {
     public void connect(int port, String host) {
 
-        NioEventLoopGroup group = new NioEventLoopGroup();
+        EventLoopGroup group = new NioEventLoopGroup();
 
         try {
             Bootstrap b = new Bootstrap();

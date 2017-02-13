c8be901c525605568599e9ba20eb065304d81978
1538417087@qq.com <wenjie123>
2017-02-13 11:16:01 -0500

    is too thoughtless ,there is new TimeClient in the if args is not null

---
+++ netty/src/main/java/basic/tcpexception/TimeClient.java
@@ -0,0 +1,55 @@
+package basic.tcpexception;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * Created by root on 17-2-13.
+ */
+public class TimeClient {
+    public void connect(int port, String host) {
+        //configure nio thread
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group).channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel socketChannel) throws Exception {
+                            socketChannel.pipeline().addLast(new TimeClientHandler());
+                        }
+                    });
+
+            //begin asyn connect
+            ChannelFuture f = b.connect(host, port).sync();
+
+            //wait client to close
+            f.channel().closeFuture().sync();
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    public static void main(String[] args) {
+        int port = 8080;
+        if (args != null && args.length > 0) {
+            try {
+                port = Integer.valueOf(args[0]);
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        new TimeClient().connect(port, "127.0.0.1");
+    }
+}

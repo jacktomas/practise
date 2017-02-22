33f5b733ba26c85024673cfae9d43a9ec3d201e5
1538417087@qq.com <wenjie123>
2017-02-22 10:48:29 -0500

    class HttpXmlClient: the  initChanel  decoder class is HttpResponseDecoder replace of HttpRequestDecoder, encoder class is HttpRequestEncoder replace of  HttpResponseEncoder

--- netty/src/main/java/protocol/httpxml/client/HttpXmlClient.java
+++ netty/src/main/java/protocol/httpxml/client/HttpXmlClient.java
@@ -9,8 +9,8 @@ import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.HttpObjectAggregator;
-import io.netty.handler.codec.http.HttpRequestDecoder;
-import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.codec.http.HttpRequestEncoder;
+import io.netty.handler.codec.http.HttpResponseDecoder;
 import protocol.httpxml.codec.HttpXmlRequestEncoder;
 import protocol.httpxml.codec.HttpXmlResponseDecoder;
 import protocol.httpxml.pojo.Order;
@@ -48,12 +48,12 @@ public class HttpXmlClient {
                         @Override
                         protected void initChannel(SocketChannel socketChannel) throws Exception {
 
-                            socketChannel.pipeline().addLast("http-decoder", new HttpRequestDecoder());
+                            socketChannel.pipeline().addLast("http-decoder", new HttpResponseDecoder());
                             socketChannel.pipeline().addLast("http-aggregator", new HttpObjectAggregator(65536));
 
 
                             socketChannel.pipeline().addLast("xml-decoder", new HttpXmlResponseDecoder(Order.class, true));
-                            socketChannel.pipeline().addLast("http-encoder", new HttpResponseEncoder());
+                            socketChannel.pipeline().addLast("http-encoder", new HttpRequestEncoder());
                             socketChannel.pipeline().addLast("xml-encoder", new HttpXmlRequestEncoder());
 
                             socketChannel.pipeline().addLast("xmlClientHandler", new HttpXmlClientHandle());

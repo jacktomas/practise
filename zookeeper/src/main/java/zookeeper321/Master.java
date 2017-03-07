aaf3ee247ec748ed7d761bc5755d896888832975
wenda <1538417087@qq.com>
2017-03-07 18:57:03 +0800

    zookeeper demo 代码

---
+++ zookeeper/src/main/java/zookeeper321/Master.java
@@ -0,0 +1,47 @@
+package zookeeper321;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+
+import java.io.IOException;
+
+/**
+ * Created by root on 17-3-7.
+ */
+public class Master implements Watcher {
+
+    ZooKeeper zooKeeper;
+    String hostport;
+
+    public Master(String hostport) {
+        this.hostport = hostport;
+    }
+
+    void startZK() throws IOException {
+        zooKeeper = new ZooKeeper(hostport, 2000, this);
+    }
+
+    void stopZK() {
+        try {
+            zooKeeper.close();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void process(WatchedEvent watchedEvent) {
+        System.out.println(watchedEvent);
+    }
+
+    public static void main(String[] args) throws IOException {
+        Master master = new Master("127.0.0.1:2181");
+        master.startZK();
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        master.startZK();
+    }
+}

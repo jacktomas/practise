9526d67bd8d0d57164d10f9f412e16168b742b5d
wenda <1538417087@qq.com>
2017-03-21 10:37:54 +0800

    reflect test

--- distribute/src/main/java/proxy/TestInterfaceImp.java
+++ distribute/src/main/java/proxy/TestInterfaceImp.java
@@ -8,12 +8,12 @@ public class TestInterfaceImp implements TestInterface {
         System.out.println(" this is testInterface object ");
     }
 
-    @Override
+
     public void dosomething() {
         System.out.println("this function 1 ");
     }
 
-    @Override
+
     public void somethingElse(String arg) {
         System.out.println("this is a real instance ");
         System.out.println("the param is function 2 : "+arg);

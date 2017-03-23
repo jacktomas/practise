60862619a9b96b15905d55ab1583bb639aa7d6f7
Administrator <1538417087@qq.com>
2017-03-24 01:24:57 +0800

    springboottest

--- testspringboot/src/main/java/com/test/TestspringbootApplication.java
+++ testspringboot/src/main/java/com/test/TestspringbootApplication.java
@@ -6,14 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
-@EnableAutoConfiguration
+@RestController
 public class TestspringbootApplication {
 
 	@RequestMapping("/")
-	@ResponseBody
 	String home() {
 		return "Hello World!";
 	}

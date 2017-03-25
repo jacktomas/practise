fb586fb7869f00645248181e4249e628168d7533
wenda <1538417087@qq.com>
2017-03-25 17:15:19 +0800

    spring boot

--- testspringboot/src/main/java/com/test/TestspringbootApplication.java
+++ testspringboot/src/main/java/com/test/TestspringbootApplication.java
@@ -1,23 +1,20 @@
 package com.test;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class TestspringbootApplication {
 
-	@RequestMapping("/")
-	String home() {
-		return "Hello World!";
-	}
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestspringbootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TestspringbootApplication.class, args);
+    }
 }

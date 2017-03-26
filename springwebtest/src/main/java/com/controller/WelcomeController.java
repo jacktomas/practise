0b3cf2d79c76c6429338bc69f5211533d4f7f542
Administrator <1538417087@qq.com>
2017-03-26 23:30:01 +0800

    spring web jsp

---
+++ springwebtest/src/main/java/com/controller/WelcomeController.java
@@ -0,0 +1,44 @@
+package com.controller;
+
+/**
+ * Created by root on 17-3-25.
+ */
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.Map;
+
+@Controller
+public class WelcomeController {
+
+
+    @RequestMapping("/hello")
+    public String hello(Model model, @RequestParam(value = "name", required = false, defaultValue = "World") String name) {
+        model.addAttribute("name", name);
+        System.out.println("name is : "+ name);
+        return "hello";
+    }
+
+
+    @Value("${application.message:Hello World}")
+    private String message = "Hello World";
+
+    @GetMapping("/")
+    public String welcome(Map<String, Object> model) {
+        model.put("time", new Date());
+        model.put("message", this.message);
+        return "welcome";
+    }
+
+    @RequestMapping("/foo")
+    public String foo(Map<String, Object> model) {
+        throw new RuntimeException("Foo");
+    }
+
+}
